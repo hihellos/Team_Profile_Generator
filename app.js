af4fb923779ce55937d60646d0a94c472d2b5716
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-17 21:10:51 -0400

    completed prompts for dingle employee entry

--- app.js
+++ app.js
@@ -10,13 +10,77 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Use inquirer to gather information about the development team members, and create objects for each team member (using the correct classes as blueprints!)
 
-// 1. Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employeesArr = [];
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the employee's name: "
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please input the employee ID: ",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter employee's email: "
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Please select this employee's role: ",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        // depending on role selected, continue prompts respective of selection 
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter manager office number: ",
+            when: (answer) => answer.role === "Manager"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter employee github username: ",
+            when: (answer) => answer.role === "Engineer"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter intern school name: ",
+            when: (answer) => answer.role === "Intern"
+        }
+    ]).then(function(content) {
+        console.log(content);
+    });
+};
+
+promptUser()
+    // .then(function(content) {
+    //     console.log("This is our employee: ", content);
+    // });
+
+// .then(function(managerAnswer) {
+//     const addManager = new Manager(
+//         answers.name, 
+//         answers.id, 
+//         answers.email, 
+//         managerAnswer.officeNumber);
+
+//     employeesArr.push(addManager);
+//     addEmployee();
+// })
+
+// const addEmployee = () =>
 
 // After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -32,8 +96,4 @@ const render = require("./lib/htmlRenderer");
 // Engineer: What is this engineer's name: What is this engineer's id: What is this engineer's email: What is this engineer's github username:
 // Intern: What is this intern's name: What is this intern's id: What is this intern's email: What is this intern's school:
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work! ```
